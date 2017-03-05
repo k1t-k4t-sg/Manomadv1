f18dca2157e4af98f625e740c6c4c067f8e02a98
Stepan <creadtive472@gmail.com>
2017-03-06 00:32:04 +1000

    tempalate-portfolio

--- index-portfolio.php
+++ index-portfolio.php
@@ -1,12 +1,11 @@
-<!-- AddThis Button END -->
-<?php $wallstreet_pro_options=theme_data_setup();
-$current_options = wp_parse_args(  get_option( 'wallstreet_pro_options', array() ), $wallstreet_pro_options ); 
- if($current_options['portfolio_section_enabled'] == true) { ?>
+<?php 
+    $wallstreet_pro_options=theme_data_setup();
+    $current_options = wp_parse_args(  get_option( 'wallstreet_pro_options', array() ), $wallstreet_pro_options );
+?>
  
  
 <div class="portfolio-section">
 	<div class="container">
-
 		<div class="row">
 			<div class="section_heading_title">
 				<!--
@@ -33,104 +32,44 @@ $current_options = wp_parse_args(  get_option( 'wallstreet_pro_options', array()
 			</div>
 		</div>
 		<div class="row">
-		    <?php if($current_options['portfolio_image_one']) { ?>
-			<div class="col-md-3 col-md-6 home-portfolio-area">
-				<div class="home-portfolio-showcase">
-					<div class="home-portfolio-showcase-media">
-					
-						<img class="img-responsive home-portfolio-img" alt="Sleek &amp; Beautiful" src="<?php echo esc_url($current_options['portfolio_image_one']); ?>">
-					
-						<div class="home-portfolio-showcase-overlay">
-							<div class="home-portfolio-showcase-overlay-inner">
-								<div class="home-portfolio-showcase-detail">
-									<?php if($current_options['portfolio_title_one']){ ?>
-									<h4><?php echo esc_html($current_options['portfolio_title_one']); ?></h4>
-									<?php } ?>
-									<?php if($current_options['portfolio_description_one']){ ?>
-									<p><?php echo esc_html($current_options['portfolio_description_one']);?></p>
-									<?php } ?>
-									
-                            
-                            
-                            
-								</div>                         
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php }                                       
-            if($current_options['portfolio_image_two']) { ?>
-			<div class="col-md-3 col-md-6 home-portfolio-area">
-				<div class="home-portfolio-showcase">
-					<div class="home-portfolio-showcase-media">
-					
-						<img class="img-responsive home-portfolio-img" alt="Sleek &amp; Beautiful" src="<?php echo esc_url($current_options['portfolio_image_two']); ?>">
-					
-						<div class="home-portfolio-showcase-overlay">
-							<div class="home-portfolio-showcase-overlay-inner">
-								<div class="home-portfolio-showcase-detail">
-									<?php if($current_options['portfolio_title_two']){ ?>
-									<h4><?php echo esc_html($current_options['portfolio_title_two']); ?></h4>
-									<?php } ?>
-									<?php if($current_options['portfolio_description_two']){ ?>
-									<p><?php echo esc_html($current_options['portfolio_description_two']);?></p>
-									<?php } ?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<?php }
-			if($current_options['portfolio_image_three']) { ?>
-			<div class="col-md-3 col-md-6 home-portfolio-area">
-				<div class="home-portfolio-showcase">
-					<div class="home-portfolio-showcase-media">
-					
-						<img class="img-responsive home-portfolio-img" alt="Sleek &amp; Beautiful" src="<?php echo esc_url($current_options['portfolio_image_three']); ?>">
+            <!--
 				
-						<div class="home-portfolio-showcase-overlay">
-							<div class="home-portfolio-showcase-overlay-inner">
-								<div class="home-portfolio-showcase-detail">
-									<?php if($current_options['portfolio_title_three']){ ?>
-									<h4><?php echo esc_html($current_options['portfolio_title_three']); ?></h4>
-									<?php } ?>
-									<?php if($current_options['portfolio_description_three']){ ?>
-									<p><?php echo esc_html($current_options['portfolio_description_three']);?></p>
-									<?php } ?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-            <?php }
-            if($current_options['portfolio_title_four']) { ?>
-			<div class="col-md-3 col-md-6 home-portfolio-area">
-				<div class="home-portfolio-showcase">
-					<div class="home-portfolio-showcase-media">
-					
-						<img class="img-responsive home-portfolio-img" alt="Sleek &amp; Beautiful" src="<?php echo esc_url($current_options['portfolio_image_four']); ?>">
+            Start CUSTOM manomadv1 (post teg "start")
 				
-						<div class="home-portfolio-showcase-overlay">
-							<div class="home-portfolio-showcase-overlay-inner">
-								<div class="home-portfolio-showcase-detail">
-									<?php if($current_options['portfolio_title_four']){ ?>
-									<h4><?php echo esc_html( $current_options['portfolio_title_four'] ); ?></h4>
-									<?php } ?>
-									<?php if($current_options['portfolio_description_four']){ ?>
-									<p><?php echo esc_html( $current_options['portfolio_description_four'] );?></p>
-									<?php } ?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-	    <?php } ?>
-		</div>
-</div>	
+            -->
+		    <?php 
+            /*
+            
+            cat=3 вывод категории 3 start
+            
+            */
+                if ( have_posts() ) : query_posts('cat=3'); 
+                while (have_posts()) : the_post();
+            ?>
+            <div class="col-md-3 col-md-6 home-portfolio-area">
+                <div class="home-portfolio-showcase">
+                    <div class="home-portfolio-showcase-media">
+                        <img class="img-responsive home-portfolio-img" alt="Sleek &amp; Beautiful" src="<?php the_post_thumbnail_url(); ?>">
+                            <div class="home-portfolio-showcase-overlay">
+                                <div class="home-portfolio-showcase-overlay-inner">
+                                    <div class="home-portfolio-showcase-detail">
+                                        <h4><?php the_title(); ?></h4>
+                                        <p><?php the_excerpt(); ?></p>
+                                    </div>
+                                    <div class="home-blog-btn">
+                                        <a href="<?php the_permalink(); ?>">Подробнее</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; endif; wp_reset_query(); ?>
+            <!--
+				
+            End CUSTOM manomadv1 (post teg "start")
+				
+            -->
+        </div>
+    </div>
 </div>
-<?php } ?>
-<!-- /wallstreet Portfolio Section ---->
