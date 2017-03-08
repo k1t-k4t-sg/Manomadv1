91caf6a1dfc42f933dfe48842fb47be8953a946e
Stepan <creadtive472@gmail.com>
2017-03-08 23:48:12 +1000

    page VIDEO HTML5

--- index-static-banner.php
+++ index-static-banner.php
@@ -7,7 +7,19 @@
 	<?php if($current_options['home_banner_enabled'] == true) { ?>
 				
 					<?php if($current_options['slider_image']){ ?>
-					<img class="img-responsive CUSTOM_manomadv" alt="Sleek &amp; Beautiful" src="/wp-content/uploads/2017/03/one.jpg">
+					<!--
+					
+					    video CUSTOM razar.mp4
+				    
+                   -->
+                    <div id="styled_video_container">
+                     <video width="100%" height="100%" loop="loop" muted="muted" autoplay="autoplay" poster="/wp-content/uploads/2017/03/one.jpg">
+                         <source src="/wp-content/uploads/2017/03/razar.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                         Тег video не поддерживается вашим браузером. 
+                         <a href="video/duel.mp4">Скачайте видео</a>.
+                    </video>
+                    </div>
+                    <!--img class="img-responsive CUSTOM_manomadv" alt="Sleek &amp; Beautiful" src="/wp-content/uploads/2017/03/one.jpg"-->
 					<?php } ?>
 					<div class="flex-slider-center">
 					<?php if($current_options['slider_title_one']){ ?>
